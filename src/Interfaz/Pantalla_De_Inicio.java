16e40cdf7343f2ee20a15a55a82edb040465f99d
Martinf170503 <martin.fuentes@udla.edu.ec>
2024-01-20 16:53:19 -0500

    Hello World

---
+++ src/Interfaz/Pantalla_De_Inicio.java
@@ -0,0 +1,18 @@
+package Interfaz;
+
+import javax.swing.*;
+
+public class Pantalla_De_Inicio {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JButton iniciarSesiónButton;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton registrarUsuarioButton;
+    private JPasswordField passwordField3;
+    private JComboBox comboBox1;
+    private JTextField textField5;
+}
